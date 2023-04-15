38c32d11a0768772560ee5131feb12249661e77f
Vince <vmorrill@appliedsystems.com>
2023-04-14 23:51:08 -0500

    Fixed botched remote -> local cloud merge Implemented All tests

---
+++ BookRating-SortSearch-Starter/BookRating/BookList.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRating
+{
+    public class BookList
+    {
+        private List<Book> allBooks;
+
+        public BookList()
+        {
+            allBooks = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            allBooks.Add(book);
+        }
+
+        public int NumberBooks()
+        {
+            return allBooks.Count;
+        }
+
+        public double HighestRating()
+        {
+            double result = 0.0;
+            foreach (Book b in allBooks)
+            {
+                if (b.Rating > result)
+                    result = b.Rating;
+            }
+            return result;
+        }
+
+        public double RatingByAuthor(string author)
+        {
+            double result = -1;
+            double sum = 0;
+            int count = 0;
+            foreach (Book b in allBooks)
+            {
+                if (b.Author.Equals(author))
+                {
+                    count++;
+                    sum += b.Rating;
+                }
+                if (count > 0)
+                    result = sum / count;
+            }
+            return result;
+        }
+        public void RemoveBook(Book book)
+        {
+            allBooks.Remove(book);
+        }
+
+
+        public double AverageRating()
+        {
+            double result = -1;
+            double sum = 0;
+            foreach (Book b in allBooks)
+            {
+                sum += b.Rating;
+            }
+            result = sum / allBooks.Count;
+            return result;
+        }
+
+        public void RemoveBook(int year)
+        {
+            List<Book> removeList = new List<Book>(); //list to hold search results
+            foreach (Book b in allBooks)
+            {
+                if (b.Year == year) //we have a hit!
+                {
+                    //allBooks.Remove(b); //can't remove inside loop
+                    removeList.Add(b); //add search result to the list
+                }
+            }
+
+            foreach (Book b in removeList) //go through each element in the search results
+            {
+                allBooks.Remove(b); //...and remove from the original list
+            }
+            //Alternative to last foreach loop:
+            // allBooks = allBooks.Except(removeList).ToList();
+        }
+
+        public void RemoveLastAdded()
+        {
+            allBooks.RemoveAt(allBooks.Count - 1);
+        }
+
+        public bool ContainsBook(Book book)
+        {
+            return allBooks.Contains(book);
+        }
+
+        public Book OldestBook()
+        {
+            Book oldestBook = allBooks[0];
+            foreach (Book b in allBooks)
+            {
+                if (b.Year < oldestBook.Year)
+                    oldestBook = b;
+            }
+            return oldestBook;
+        }
+
+        public object YoungestBook()
+        {
+            Book youngestBook = allBooks[0];
+            foreach (Book b in allBooks)
+            {
+                if (b.Year > youngestBook.Year)
+                    youngestBook = b;
+            }
+            return youngestBook;
+        }
+        public List<Book> SortedByAuthor(bool sortDirection = false)
+        {
+            allBooks.Sort();
+
+            if (!sortDirection)
+            {
+                // List<T>.Sort() always sorts Ascending, so if we need to flip it...
+                allBooks.Reverse();
+            }
+            return allBooks;
+        }
+
+        public List<Book> AboveRating(double rating, string targetAuthor)
+        {
+            // Return an Enumerable eq
+            allBooks.Where(book =>
+                book.Author == targetAuthor).ToList();
+
+
+            return allBooks.Where(b =>
+            b.Rating >= rating).ToList();
+        }
+
+        public List<Book> SortedByAuthorYear(bool sortDirection)
+        {
+            // Bet you didn't think we could do this in one line!
+            List<Book> sortedList = allBooks.OrderBy(book => book.Author).ThenBy(book => book.Year).ToList();
+            return sortedList;
+            /**
+            allBooks = SortedByAuthor(sortDirection);
+            // do we need to implement another CompareTo that uses year instead of the 
+            //EqualityComparer<Book>.Default
+
+            SortByAuthor(sortDirection);
+
+            // organize by the year
+            HashSet<char> startsWith = new HashSet<char>();
+            // count of books by this letter, the letter in question
+
+            // use a bucket for each letter we have in the table...
+            allBooks.ForEach(b => startsWith.Add(b.Author[0]));
+
+            // now, we can just sort each "bucket" by year!
+            int greatestYear = int.MinValue; */
+        }
+
+        private void SortByYear(bool sortDirection = false)
+        {
+            for (int i = 1; i < allBooks.Count; i++)
+            {
+                // if the i-th book was published more recently than book at index 0..
+                if (sortDirection)
+                {
+
+                    // Take the largest year between 0-th year and i-th year
+                    if (Math.Min(allBooks[i].Year, allBooks[0].Year) == allBooks[i].Year)
+                    {
+                        /// update it
+                        Book temp = allBooks[0];
+                        allBooks[0] = allBooks[i];
+                        allBooks[i] = temp;
+                    }
+
+                }
+                else
+                {
+                    if (allBooks[i].Year < allBooks[0].Year)
+                    {
+                        /// update it
+                        Book temp = allBooks[0];
+                        allBooks[0] = allBooks[i];
+                        allBooks[i] = temp;
+                    }
+
+                }
+
+            }
+        }
+
+        private void SortByAuthor(bool sortDirection)
+        {
+
+            for (int i = 1; i < allBooks.Count; i++)
+            {
+                if (sortDirection)
+                {
+                    if (allBooks[i].Author.CompareTo(allBooks[0].Author) == -1)
+                    {
+                        Book temp = allBooks[i];
+                        allBooks[0] = allBooks[i];
+                        allBooks[i] = temp;
+                    }
+                }
+                else
+                {
+                    if (allBooks[i].Author.CompareTo(allBooks[0].Author) == 1)
+                    {
+                        Book temp = allBooks[i];
+                        allBooks[0] = allBooks[i];
+                        allBooks[i] = temp;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ BookRating-SortSearch-Starter/BookRatingTests/BookTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookRating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRating.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        BookList books;
+        Book b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
+
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            b1 = new Book("A Tale of Two cities", "Charles Dickens", 1859, 3.76);
+            b2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, 3.85);
+            b3 = new Book("Pride and Prejudice", "Jane Austen", 1813, 4.23);
+            b4 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, 4.23);
+            b5 = new Book("Jane Eyre", "Charlotte Bronte", 1847, 4.07);
+            b6 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951, 3.77);
+            b7 = new Book("Sense and Sensibility", "Jane Austen", 1811, 4.04);
+            b8 = new Book("Emma", "Jane Austen", 1815, 3.97);
+            b9 = new Book("1984", "George Orwell", 1949, 4.09);
+            b10 = new Book("Oliver Twist", "Charles Dickens", 1838, 3.82);
+
+            books = new BookList();
+            books.AddBook(b1);
+            books.AddBook(b2);
+            books.AddBook(b3);
+            books.AddBook(b4);
+            books.AddBook(b5);
+            books.AddBook(b6);
+            books.AddBook(b7);
+            books.AddBook(b8);
+            books.AddBook(b9);
+            books.AddBook(b10);
+
+
+        }
+        [TestMethod]
+        public void BooksAddedTest()
+        {
+            Assert.AreEqual(10, books.NumberBooks());
+            books.AddBook(new Book("Frankenstein", "Mary Shelley", 1818, 3.76));
+            Assert.AreEqual(11, books.NumberBooks());
+
+        }
+
+        [TestMethod]
+        public void HighestRatingTest()
+        {
+            Assert.AreEqual(4.23, books.HighestRating());
+        }
+
+        [TestMethod]
+        public void RemoveBookTest()
+        {
+            books.RemoveBook(b5);
+            Assert.AreEqual(9, books.NumberBooks());
+        }
+
+        [TestMethod]
+        public void RemoveBookByYearTest()
+        {
+            books.RemoveBook(1815);
+            Assert.AreEqual(9, books.NumberBooks());
+        }
+
+
+        [TestMethod]
+        public void RemoveLastAddTest()
+        {
+            books.RemoveLastAdded();
+            Assert.IsFalse(books.ContainsBook(b10));
+        }
+        [TestMethod]
+        public void AverageRatingTest()
+        {
+            Assert.AreEqual(3.98, books.AverageRating(), .005);
+        }
+
+        [TestMethod]
+        public void AverageByAuthorTest()
+        {
+            Assert.AreEqual((4.23 + 4.04 + 3.97) / 3.0, books.RatingByAuthor("Jane Austen"));
+            Assert.AreEqual(-1, books.RatingByAuthor("Jakob Iversen")); //Author doesn't exist in the list of books
+        }
+
+        [TestMethod]
+        public void OldestBookTest()
+        {
+            Assert.AreSame(b7, books.OldestBook());
+        }
+
+        [TestMethod]
+        public void YoungestBookTest()
+        {
+            Assert.AreSame(b4, books.YoungestBook());
+        }
+
+        [TestMethod]
+        public void SortByAuthorTest()
+        {
+            List<Book> booksByAuthor = new List<Book> { b1, b10, b5, b2, b9, b4, b6, b3, b7, b8 };
+            List<Book> booksByAuthorReverse = new List<Book> { b8, b7, b3, b6, b4, b9, b2, b5, b10, b1 };
+            CollectionAssert.AreEqual(booksByAuthor, books.SortedByAuthor(true)); //ascending sort
+            CollectionAssert.AreEqual(booksByAuthorReverse, books.SortedByAuthor(false)); //descending sort
+        }
+
+        [TestMethod]
+        public void SortByAuthorYearTest()
+        {
+            List<Book> booksByAuthorYear = new List<Book> { b10, b1, b5, b2, b9, b4, b6, b7, b3, b8 };
+            CollectionAssert.AreEqual(booksByAuthorYear, books.SortedByAuthorYear(true));
+        }
+
+        [TestMethod]
+        public void FourPlusTest()
+        {
+            List<Book> fourPlus = new List<Book> { b3, b4, b5, b7, b9 };
+            CollectionAssert.AreEquivalent(fourPlus, books.AboveRating(4.0, "Jane Austen"));
+        }
+    }
+}
